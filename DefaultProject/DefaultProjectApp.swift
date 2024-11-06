42665486bd1c811fbb96ec3d744a2b2beaa47a0e
annderevyankina <anka73der@gmail.com>
2024-11-06 12:20:07 +0300

    Initial Commit

---
+++ DefaultProject/DefaultProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DefaultProjectApp.swift
+//  DefaultProject
+//
+//  Created by Анна on 06.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct DefaultProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
